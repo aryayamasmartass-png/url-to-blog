ed2b3c4cfd4af8626db7fec2c7d45da81d7c5dbc
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2026-01-07 13:55:08 +0530

    fixed bugs

--- app/build.sh
+++ app/build.sh
@@ -2,8 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Install Python dependencies
-pip install -r requirements.txt
+# Install uv for faster dependency resolution
+pip install uv
+
+# Install Python dependencies using uv (much faster than pip)
+uv pip install --system -r requirements.txt
 
 # Install Playwright browsers (Required by crawl4ai on Linux)
 python -m playwright install --with-deps chromium
